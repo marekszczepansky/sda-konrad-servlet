69f637e626ae32a1b1271bf628b8eabac96f182a
marekszczepansky <marek.szczepansky@gmail.com>
2019-03-09 15:09:47 +0100

    Zadanie 1 - cookie

---
+++ src/main/java/sda/javapoz12/filters/RedirectStartFilter.java
@@ -0,0 +1,52 @@
+package sda.javapoz12.filters;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Optional;
+
+@WebFilter(filterName = "Redirect to start", urlPatterns = "/task/*")
+public class RedirectStartFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        boolean visited = false;
+
+        if (req instanceof HttpServletRequest) {
+            HttpServletRequest request;
+            request = (HttpServletRequest) req;
+
+            Cookie[] cookies = request.getCookies();
+            visited = Arrays.stream(cookies)
+                    .filter(cookie -> "visited".equals(cookie.getName()) && "yes".equals(cookie.getValue()))
+                    .findFirst().isPresent();
+        }
+
+        if (resp instanceof HttpServletResponse) {
+            HttpServletResponse response;
+            response = (HttpServletResponse) resp;
+
+            if (!visited) {
+                response.sendRedirect("/index.jsp");
+            }
+        }
+
+        if (visited) {
+            chain.doFilter(req, resp);
+        }
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+}
